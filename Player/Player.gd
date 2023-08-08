c46b8e47c16b6d74e1ee676ec207851041144ff5
Mike Depinet <mike.depinet@gmail.com>
2023-08-08 00:03:41 -0700

    P9 - Attacking Animation

--- Player/Player.gd
+++ Player/Player.gd
@@ -4,6 +4,13 @@ const ACCELERATION = 500
 const DECELERATION = 500
 const MAX_SPEED = 80
 
+enum {
+	MOVE,
+	ROLL,
+	ATTACK
+}
+
+var state = MOVE
 var target_velocity = Vector2.ZERO
 
 @onready
@@ -11,15 +18,28 @@ var animationTree = $AnimationTree
 @onready
 var animationState = animationTree.get("parameters/playback")
 
+func _ready():
+	animationTree.active = true
+
 func _physics_process(delta):
+	match state:
+		MOVE:
+			move_state(delta)
+		ROLL:
+			pass
+		ATTACK:
+			attack_state(delta)
+
+func move_state(delta):
 	var input_vector = Vector2.ZERO
-	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	input_vector.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
 		animationTree.set("parameters/Idle/blend_position", input_vector)
 		animationTree.set("parameters/Run/blend_position", input_vector)
+		animationTree.set("parameters/Attack/blend_position", input_vector)
 		animationState.travel("Run")
 		target_velocity = target_velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
@@ -29,4 +49,15 @@ func _physics_process(delta):
 	velocity = target_velocity
 	move_and_slide()
 	
-	
+	if Input.is_action_just_pressed("attack"):
+		state = ATTACK
+
+func attack_state(delta):
+	target_velocity = target_velocity.move_toward(Vector2.ZERO, DECELERATION * delta)
+	velocity = target_velocity
+	move_and_slide()
+	animationState.travel("Attack")
+
+func animation_finished(animation_name):
+	state = MOVE
+
